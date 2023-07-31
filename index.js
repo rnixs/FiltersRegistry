067a970ef32356ca3a4b8cded34e4aebfc72fa04
Maxim Topciu <mtopciu@adguard.com>
2023-07-31 13:08:15 +0300

    AG-18623 add file for partial report

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* globals require, __dirname, process */
+const { formatDate } = require('./utils/strings');
 
 let whitelist = [];
 let blacklist = [];
@@ -23,8 +24,8 @@ const logPath = path.join(__dirname, './log.txt');
 
 let reportPath = path.join(__dirname, './report.txt');
 if (whitelist.length > 0 || blacklist.length > 0) {
-    // Disable report if this is not a full build
-    reportPath = null;
+    // report_DD-MM-YYYY_HH-MM-SS.txt
+    reportPath = path.join(__dirname, `./report_${formatDate(new Date())}.txt`);
 }
 
 const customPlatformsConfig = require('./custom_platforms.js');
