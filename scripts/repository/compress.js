b52d6ac2b8bcd6b83ae77f9969c910cc56b69478
Dmitry Seregin <105th@users.noreply.github.com>
2023-12-28 13:01:26 +0300

    Fix compress script (#882)
    fixes

--- scripts/repository/compress.js
+++ scripts/repository/compress.js
@@ -20,28 +20,33 @@ if (firstArgument) {
 async function squashAndPush() {
     const git = simpleGit();
 
+    let headBeforeSquashHash = await git.revparse(['HEAD']);
+    headBeforeSquashHash = headBeforeSquashHash.trim();
+
     // Step 1: Checkout to the commitsToKeep'th commit and save its hash
     await git.checkout(`HEAD~${commitsToKeep}`);
-    const squashedCommitHash = await git.raw([
+    let squashedCommitHash = await git.raw([
         'rev-parse',
         'HEAD',
     ]);
-    console.log(`Step 1: Checked out to commit ${squashedCommitHash.trim()}`);
+    squashedCommitHash = squashedCommitHash.trim();
+    console.log(`Step 1: Checked out to commit ${squashedCommitHash}`);
 
     // Step 2: Create a new branch named 'squashed'
-    await git.checkoutBranch('squashed', squashedCommitHash.trim());
+    await git.checkoutBranch('squashed', squashedCommitHash);
     console.log('Step 2: Created branch "squashed"');
 
     // Step 3: Get the hash of the very first commit
-    const firstCommitHash = await git.raw([
+    let firstCommitHash = await git.raw([
         'rev-list',
         '--max-parents=0',
         'HEAD',
     ]);
-    console.log(`Step 3: Retrieved hash of the first commit: ${firstCommitHash.trim()}`);
+    firstCommitHash = firstCommitHash.trim();
+    console.log(`Step 3: Retrieved hash of the first commit: ${firstCommitHash}`);
 
     // Step 4: Drop all directories to the very first commit
-    await git.reset(['--mixed', firstCommitHash.trim()]);
+    await git.reset(['--mixed', firstCommitHash]);
     console.log('Step 4: Dropped all directories to the first commit');
 
     // Step 5: Add everything to the index
@@ -57,15 +62,15 @@ async function squashAndPush() {
     git.env('GIT_COMMITTER_DATE', squashedCommitDate);
 
     // Step 6: Create a commit for squashed history
-    await git.commit(`squashed history from ${firstCommitHash.trim()} to ${squashedCommitHash.trim()}`);
+    await git.commit(`squashed history from ${firstCommitHash} to ${squashedCommitHash}`);
     // eslint-disable-next-line max-len
-    console.log(`Step 6: Created commit for squashed history from ${firstCommitHash.trim()} to ${squashedCommitHash.trim()}`);
+    console.log(`Step 6: Created commit for squashed history from ${firstCommitHash} to ${squashedCommitHash}`);
 
     // Step 7: Cherry-pick the commits you want to store
     // Use the `log` method with a range specification to get the commit history
     const historyToSave = await git.log({
-        from: `master~${commitsToKeep}`,
-        to: 'master',
+        from: squashedCommitHash,
+        to: headBeforeSquashHash,
         '--no-merges': true,
     });
     const commits = historyToSave.all.reverse();
@@ -99,17 +104,7 @@ async function squashAndPush() {
     await git.reset(['--hard', 'squashed']);
     console.log('Step 9: Reset "master" to the new rebased "master"');
 
-    // Step 10: Push with --force to overwrite the remote 'master' branch
-    await git.addConfig('user.name', 'filters@adguard.com');
-    await git.addConfig('user.email', 'Filters Builder');
-    await git.push(['--set-upstream', 'origin', '--force', 'master']);
-    console.log('Step 10: Pushed with --force to overwrite the remote "master" branch');
-
-    // Step 11: Clean space with aggressive garbage collection
-    await git.raw(['gc', '--aggressive', '--prune=now']);
-    console.log('Step 11: Cleaned space with aggressive garbage collection');
-
-    console.log('Git actions completed successfully.');
+    console.log('Git actions for compression completed successfully.');
 }
 
 // Execute the squashAndPush function
