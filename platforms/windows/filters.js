0af36c92cb3cbce2f9ee485589f1867127a192a6
Filters Builder <filters@adguard.com>
2023-12-31 06:36:00 +0000

    skip ci. build from Sun Dec 31 06:36:00 UTC 2023

--- platforms/windows/filters.js
+++ platforms/windows/filters.js
@@ -607,8 +607,8 @@
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/15.txt",
 			"trustLevel": "full",
-			"version": "2.0.59.89",
-			"timeUpdated": "2023-12-31T00:31:34+0000",
+			"version": "2.0.59.90",
+			"timeUpdated": "2023-12-31T06:31:30+0000",
 			"languages": [],
 			"tags": [
 				45,
