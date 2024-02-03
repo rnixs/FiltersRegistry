ebbb7a3f416b81402b80282d8b86211f16b7a7e5
Filters Builder <filters@adguard.com>
2024-02-03 14:36:31 +0000

    skip ci. build from Sat Feb  3 14:36:31 UTC 2024

--- platforms/extension/safari/filters.js
+++ platforms/extension/safari/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/safari/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.20.61",
-			"timeUpdated": "2024-02-03T13:37:01+0000",
+			"version": "2.3.20.62",
+			"timeUpdated": "2024-02-03T14:32:46+0000",
 			"languages": [],
 			"tags": [
 				10,
