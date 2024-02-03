1c12449227256ac220397721b3ef351215db3a66
Filters Builder <filters@adguard.com>
2024-02-03 19:36:27 +0000

    skip ci. build from Sat Feb  3 19:36:27 UTC 2024

--- platforms/extension/ublock/filters.js
+++ platforms/extension/ublock/filters.js
@@ -636,8 +636,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/ublock/filters/16.txt",
 			"trustLevel": "full",
-			"version": "2.0.43.22",
-			"timeUpdated": "2024-02-03T04:32:05+0000",
+			"version": "2.0.43.23",
+			"timeUpdated": "2024-02-03T19:31:47+0000",
 			"languages": [
 				"fr"
 			],
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -254,8 +254,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/16.txt",
-			"version": "2.0.43.22",
-			"timeUpdated": "2024-02-03T04:32:05+0000",
+			"version": "2.0.43.23",
+			"timeUpdated": "2024-02-03T19:31:47+0000",
 			"languages": [
 				"fr"
 			]
