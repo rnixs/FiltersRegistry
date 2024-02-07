431d818ca436018d5509e0b9c6ef588ed826da95
Filters Builder <filters@adguard.com>
2024-02-07 05:36:49 +0000

    skip ci. build from Wed Feb  7 05:36:49 UTC 2024

--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -1018,8 +1018,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/224.txt",
-			"version": "2.1.10.6",
-			"timeUpdated": "2024-02-07T02:35:38+0000",
+			"version": "2.1.10.7",
+			"timeUpdated": "2024-02-07T05:32:40+0000",
 			"languages": [
 				"zh"
 			]
