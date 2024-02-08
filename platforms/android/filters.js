4be458b4d2080cfea37c13d547dc0137afeee5e8
Filters Builder <filters@adguard.com>
2024-02-08 00:36:48 +0000

    skip ci. build from Thu Feb  8 00:36:48 UTC 2024

--- platforms/android/filters.js
+++ platforms/android/filters.js
@@ -607,8 +607,8 @@
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/android/filters/15_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.0.61.52",
-			"timeUpdated": "2024-02-07T18:31:48+0000",
+			"version": "2.0.61.53",
+			"timeUpdated": "2024-02-08T00:31:54+0000",
 			"languages": [],
 			"tags": [
 				45,
--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -607,8 +607,8 @@
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/15.txt",
 			"trustLevel": "full",
-			"version": "2.0.61.52",
-			"timeUpdated": "2024-02-07T18:31:48+0000",
+			"version": "2.0.61.53",
+			"timeUpdated": "2024-02-08T00:31:54+0000",
 			"languages": [],
 			"tags": [
 				45,
