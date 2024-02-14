aa6a90c15e9a79ceead06dcfd93777fb7bbe377b
Filters Builder <filters@adguard.com>
2024-02-14 18:38:33 +0000

    skip ci. build from Wed Feb 14 18:38:33 UTC 2024

--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.22.35",
-			"timeUpdated": "2024-02-14T16:32:52+0000",
+			"version": "2.3.22.36",
+			"timeUpdated": "2024-02-14T18:33:02+0000",
 			"languages": []
 		},
 		{
@@ -74,8 +74,8 @@
 			"displayNumber": 1,
 			"groupId": 2,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/3.txt",
-			"version": "2.0.66.72",
-			"timeUpdated": "2024-02-14T14:32:57+0000",
+			"version": "2.0.66.73",
+			"timeUpdated": "2024-02-14T18:33:10+0000",
 			"languages": []
 		},
 		{
@@ -87,8 +87,8 @@
 			"displayNumber": 1,
 			"groupId": 3,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/4.txt",
-			"version": "2.0.97.32",
-			"timeUpdated": "2024-02-14T13:33:03+0000",
+			"version": "2.0.97.33",
+			"timeUpdated": "2024-02-14T18:33:16+0000",
 			"languages": []
 		},
 		{
@@ -228,8 +228,8 @@
 			"displayNumber": 1,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/14.txt",
-			"version": "2.1.87.72",
-			"timeUpdated": "2024-02-14T17:31:52+0000",
+			"version": "2.1.87.73",
+			"timeUpdated": "2024-02-14T18:32:05+0000",
 			"languages": []
 		},
 		{
@@ -241,8 +241,8 @@
 			"displayNumber": 3,
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/15.txt",
-			"version": "2.0.61.81",
-			"timeUpdated": "2024-02-14T12:32:14+0000",
+			"version": "2.0.61.82",
+			"timeUpdated": "2024-02-14T18:32:06+0000",
 			"languages": []
 		},
 		{
@@ -287,8 +287,8 @@
 			"displayNumber": 2,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/18.txt",
-			"version": "2.0.33.54",
-			"timeUpdated": "2024-02-14T17:32:03+0000",
+			"version": "2.0.33.55",
+			"timeUpdated": "2024-02-14T18:32:15+0000",
 			"languages": []
 		},
 		{
