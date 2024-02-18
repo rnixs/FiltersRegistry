a02e8c9e73e279a002a6d90239318c28dd4a1625
Filters Builder <filters@adguard.com>
2024-02-18 22:37:07 +0000

    skip ci. build from Sun Feb 18 22:37:07 UTC 2024

--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.22.96",
-			"timeUpdated": "2024-02-18T20:32:40+0000",
+			"version": "2.3.22.97",
+			"timeUpdated": "2024-02-18T22:33:04+0000",
 			"languages": []
 		},
 		{
@@ -228,8 +228,8 @@
 			"displayNumber": 1,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/14.txt",
-			"version": "2.1.88.12",
-			"timeUpdated": "2024-02-18T19:31:47+0000",
+			"version": "2.1.88.13",
+			"timeUpdated": "2024-02-18T22:32:08+0000",
 			"languages": []
 		},
 		{
@@ -300,8 +300,8 @@
 			"displayNumber": 2,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/19.txt",
-			"version": "2.0.31.79",
-			"timeUpdated": "2024-02-17T13:31:59+0000",
+			"version": "2.0.31.80",
+			"timeUpdated": "2024-02-18T22:32:21+0000",
 			"languages": []
 		},
 		{
--- platforms/windows/filters.js
+++ platforms/windows/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.22.96",
-			"timeUpdated": "2024-02-18T20:32:40+0000",
+			"version": "2.3.22.97",
+			"timeUpdated": "2024-02-18T22:33:04+0000",
 			"languages": [],
 			"tags": [
 				10,
@@ -588,8 +588,8 @@
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/14.txt",
 			"trustLevel": "full",
-			"version": "2.1.88.12",
-			"timeUpdated": "2024-02-18T19:31:47+0000",
+			"version": "2.1.88.13",
+			"timeUpdated": "2024-02-18T22:32:08+0000",
 			"languages": [],
 			"tags": [
 				5,
@@ -703,8 +703,8 @@
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/19.txt",
 			"trustLevel": "full",
-			"version": "2.0.31.79",
-			"timeUpdated": "2024-02-17T13:31:59+0000",
+			"version": "2.0.31.80",
+			"timeUpdated": "2024-02-18T22:32:21+0000",
 			"languages": [],
 			"tags": [
 				5,
