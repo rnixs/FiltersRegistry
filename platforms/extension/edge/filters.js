a0ab4e97f589ccee294a0bada86f1e5739a776b3
Filters Builder <filters@adguard.com>
2024-02-28 19:37:21 +0000

    skip ci. build from Wed Feb 28 19:37:20 UTC 2024

--- platforms/extension/edge/filters.js
+++ platforms/extension/edge/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/edge/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.24.53",
-			"timeUpdated": "2024-02-28T18:33:26+0000",
+			"version": "2.3.24.54",
+			"timeUpdated": "2024-02-28T19:33:09+0000",
 			"languages": [],
 			"tags": [
 				10,
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.24.53",
-			"timeUpdated": "2024-02-28T18:33:26+0000",
+			"version": "2.3.24.54",
+			"timeUpdated": "2024-02-28T19:33:09+0000",
 			"languages": []
 		},
 		{
